88231b336910b94f8ce7e966604a3bb180ebe917
jie <jhou.jheng.jie@gmail.com>
2023-03-10 13:45:53 +0800

    fix(react): uno shortcuts

--- packages/react/uno.config.js
+++ packages/react/uno.config.js
@@ -2,4 +2,8 @@ import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [presetUno()],
+  shortcuts: {
+    'text-white': 'text-white dark:text-#fefddd',
+    'bg-white': 'bg-white dark:bg-#fefddd',
+  },
 })
