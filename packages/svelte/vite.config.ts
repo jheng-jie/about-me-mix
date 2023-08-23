15fd36d95680d8748c39429fc14f564862ef3402
jie <jhou.jheng.jie@gmail.com>
2023-08-23 18:22:15 +0800

    feat: svelte + unocss upgrade

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
-import { extractorSvelte } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 import { StaticServicePlugin } from '@about-me-mix/common/static-service'
 import dotenv from 'dotenv'
@@ -26,7 +25,6 @@ export default () => {
           // for --at-apply:
           transformerDirectives(),
         ],
-        extractors: [extractorSvelte],
       }),
       // 自製靜態目錄代理
       StaticServicePlugin({
